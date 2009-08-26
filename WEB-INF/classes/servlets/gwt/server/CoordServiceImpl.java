cff1b3a4287a26466158133b8aca75e3bdb30301
Kevin Horan <khoran@cs.ucr.edu>
2009-08-26 18:31:24 +0000

    untested code to parse polygon string output

--- WEB-INF/classes/servlets/gwt/server/CoordServiceImpl.java
+++ WEB-INF/classes/servlets/gwt/server/CoordServiceImpl.java
@@ -10,11 +10,15 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.sql.SQLException;
 import java.util.LinkedList;
 import java.util.List;
 import org.apache.log4j.Logger;
+import servlets.DbConnection;
+import servlets.DbConnectionManager;
 import servlets.gwt.client.CoordService;
+import servlets.querySets.QuerySetProvider;
+import servlets.querySets.V1ImageMapQuerySet;
 
 /**
  *
@@ -24,6 +28,83 @@ public class CoordServiceImpl extends RemoteServiceServlet implements CoordServi
 {
     private static final Logger log=Logger.getLogger(CoordServiceImpl.class);
 
+	CoordServiceImpl()
+	{
+		if(QuerySetProvider.getImageMapQuerySet() == null)
+			QuerySetProvider.setImageMapQuerySet(new V1ImageMapQuerySet());
+	}
+
+	public int[][][][] getPolygons(int image_id)
+	{
+		int[][][][] polys=null;
+		DbConnection dbc = DbConnectionManager.getConnection("khoran");
+
+		try{
+
+			// first get some info about how many areas and components we have
+			String query = QuerySetProvider.getImageMapQuerySet().getPolygonCountQuery(image_id);
+			List results = dbc.sendQuery(query,false);
+
+			int i=0,size;
+			polys = new int[results.size()][][][];
+			for(Object o : results)  //each row has experiment_id,  number of components
+			{
+				List row = (List)o;
+				//size = Integer.parseInt((String)row.get(2));
+				size = (Integer)row.get(2);
+				polys[i++]=new int[size][][];
+			}
+
+			// then fetch and parse the actual areas
+			query = QuerySetProvider.getImageMapQuerySet().getPolygonQuery(image_id);
+			results = dbc.sendQuery(query);
+
+			String prevExpId=null;
+			String[] tokens,coord;
+			int currentArea=0,currentComponent=0,x,y;
+
+			for(Object o : results)  //each row has experiment_id, and a polygon
+			{
+				List row = (List)o;
+				tokens = stripOuterParinths((String)row.get(1)).split(",");
+				polys[currentArea][currentComponent] = new int[tokens.length][2];
+
+				for(int j=0; j < tokens.length; j++)
+				{
+					coord = stripOuterParinths(tokens[j]).split(",");
+					x =  Integer.parseInt(coord[0]);
+					y = Integer.parseInt(coord[1]);
+					polys[currentArea][currentComponent][j][0]=x;
+					polys[currentArea][currentComponent][j][1]=y;
+
+				}
+
+				if(prevExpId != null || ! prevExpId.equals((String)row.get(0)))
+				{
+					currentArea++;
+					currentComponent=0;
+				}
+				currentComponent++;
+				prevExpId = (String)row.get(0);
+			}
+		}catch(SQLException e){
+			log.error("failed to fetch polygons: "+e,e);
+		}
+
+		return polys;
+	}
+	final static String stripOuterParinths(String s)
+	{
+		int l = s.length();
+		if( l  > 1 && s.charAt(0)=='(')
+		{
+			s=s.substring(1);
+			l--;
+		}
+		if( l > 1 && s.charAt(l-1)==')')
+			s=s.substring(0, l-1);
+		return s;
+	}
 	public int[][][][] getPolygons()
 	{
 
